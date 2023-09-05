93f2bf49c780a7a326b855741b217d5711b4dbb6
Anton Harbers <Anton.Harbers23@gmail.com>
2023-09-05 12:29:18 +0400

    Feat: Added Gameboard class with TDD

--- battleships.js
+++ battleships.js
@@ -1,30 +1,108 @@
-class Ship{
-    constructor(name, length){
-        this.name = name,
-        this.length = length,
-        this.hits = 0,
-        this.sunk = false
-    }
+class Ship {
+  constructor(name, length) {
+    (this.name = name),
+      (this.length = length),
+      (this.hits = 0),
+      (this.sunk = false);
+  }
 
-    hit(){
-        this.hits++;
-    }
+  hit() {
+    this.hits++;
+    this.isSunk();
+  }
 
-    isSunk(){
-        if(this.hits == this.length){
-            this.sunk = true;
-        }
+  isSunk() {
+    if (this.hits == this.length) {
+      this.sunk = true;
     }
+  }
 }
 
-class Gameboard{
+class Gameboard {
+  constructor() {
+    this.board = new Array();
+    this.ships = [];
+    this.sunkShips = [];
+    this.gridSize = 10;
+    this.hitAttacks = [];
+    this.missedAttacks = [];
+    this.initBoard();
+  }
 
+  initBoard() {
+    for (var x = 0; x < this.gridSize; x++) {
+      this.board[x] = [];
+      for (var y = 0; y < this.gridSize; y++) {
+        this.board[x][y] = 'Empty';
+      }
+    }
+  }
+
+  placeShip(name, length, x, y, isHorizontal) {
+    for (var t = 0; t < length; t++) {
+      if (
+        this.board[isHorizontal ? x : x + t][isHorizontal ? y + t : y] !==
+        'Empty'
+      ) {
+        return false;
+      }
+    }
+    this.ships.push(new Ship(name, length));
+
+    this.board[x][y] = this.ships[this.ships.length - 1];
+
+    for (var i = 1; i < length; i++) {
+      this.board[isHorizontal ? x : x + i][isHorizontal ? y + i : y] =
+        this.ships[this.ships.length - 1];
+    }
+
+    return true;
+  }
+
+  receiveAttack(x, y) {
+    let hasAlreadyHitPosition = false;
+    this.hitAttacks.forEach((e) => {
+      if (e[0] == x && e[1] == y) {
+        hasAlreadyHitPosition = true;
+      }
+    });
+
+    this.missedAttacks.forEach((e) => {
+      console.log(e);
+      if (e[0] == x && e[1] == y) {
+        hasAlreadyHitPosition = true;
+      }
+    });
+
+    if (hasAlreadyHitPosition) return false;
+
+    if (this.board[x][y] !== 'Empty') {
+      console.log('hit');
+      this.hitAttacks.push([x, y]);
+      this.board[x][y].hit();
+    } else {
+      this.missedAttacks.push([x, y]);
+    }
+
+    return true;
+  }
+
+  allShipsSunk() {
+    let allSunk = this.ships.length === 0 ? false : true;
+
+
+    this.ships.forEach((ship) => {
+      if (!ship.sunk) {
+        console.log('issue')
+        allSunk = false;
+      }
+    });
+
+    return allSunk;
+  }
 }
 
-class Player{
+class Player {}
 
-}
-
-console.log("Works!");
 
 export { Ship, Gameboard, Player };
--- battleships.test.js
+++ battleships.test.js
@@ -1,29 +1,148 @@
-import { Ship, Gameboard, Player } from "./battleships";
+import { Ship, Gameboard, Player } from './battleships';
 
-describe("Ship class", () => {
-    test("#constructor", () => {
-        const ship = new Ship("Carrier", 5);
-        expect(ship.name).toBe("Carrier");
-        expect(ship.length).toBe(5);
-        expect(ship.hits).toBe(0);
-        expect(ship.sunk).toBe(false);
+describe('Ship class', () => {
+  test('#constructor', () => {
+    const ship = new Ship('Carrier', 5);
+    expect(ship.name).toBe('Carrier');
+    expect(ship.length).toBe(5);
+    expect(ship.hits).toBe(0);
+    expect(ship.sunk).toBe(false);
+  });
+
+  test('#hit', () => {
+    const ship = new Ship('Carrier', 5);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test('#isSunk', () => {
+    const ship = new Ship('Carrier', 5);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.isSunk();
+    expect(ship.sunk).toBe(true);
+  });
+});
+
+describe('Gameboard class', () => {
+  test('#initBoard', () => {
+    const board = new Gameboard();
+    expect(board.board[0][1]).toBe('Empty');
+    expect(board.board[1][4]).toBe('Empty');
+    expect(board.board[2][7]).toBe('Empty');
+    expect(board.board[4][8]).toBe('Empty');
+    expect(board.board.length).toBe(10);
+    expect(board.board[0].length).toBe(10);
+
+    const accessOutOfBounds = () => board.board[11][1];
+    expect(accessOutOfBounds).toThrowError();
+  });
+
+  describe('#placeShip', () => {
+    test('A ship is instantiated horizontally when calling place ship', () => {
+      const board = new Gameboard();
+      const name = 'Carrier';
+      const length = 3;
+      const x = 5;
+      const y = 2;
+      const isHorizontal = true;
+      board.placeShip(name, length, x, y, isHorizontal);
+      const ships = board.ships;
+
+      expect(board.board[x][y]).toEqual(ships[0]);
+      expect(board.board[x][y + 2]).toEqual(ships[0]);
+      expect(board.board[x + 1][y]).toBe('Empty');
     });
 
-    test("#hit", () => {
-        const ship = new Ship("Carrier", 5);
-        ship.hit();
-        expect(ship.hits).toBe(1);
+    test('A ship is instantiated vertically when calling place ship', () => {
+      const board = new Gameboard();
+      const name = 'Carrier';
+      const length = 3;
+      const x = 5;
+      const y = 2;
+      const isHorizontal = false;
+      board.placeShip(name, length, x, y, isHorizontal);
+      const ships = board.ships;
+
+      expect(board.board[x][y]).toEqual(ships[0]);
+      expect(board.board[x + 2][y]).toEqual(ships[0]);
+      expect(board.board[x][y + 1]).toBe('Empty');
     });
 
-    test("#isSunk", () => {
-        const ship = new Ship("Carrier", 5);
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        ship.isSunk();
-        expect(ship.sunk).toBe(true);
+    test('Returns false if trying to place a ship that would intersect with an already placed ship', () => {
+      const board = new Gameboard();
+
+      board.placeShip('Sub', 3, 5, 5, true);
+
+      const test1 = board.placeShip('Sub', 3, 5, 5, false);
+      const test2 = board.placeShip('Sub', 3, 5, 7, true);
+      const test3 = board.placeShip('Sub', 3, 1, 1, true);
+
+      expect(test1).toBe(false);
+      expect(test2).toBe(false);
+      expect(test3).toBe(true);
+    });
+  });
+
+  describe('#receiveAttack', () => {
+    test('Given the coordinates of a ship, call the hit function on that ship', () => {
+      const board = new Gameboard();
+      board.placeShip('Sub', 3, 5, 5, true);
+
+      board.receiveAttack(5, 5);
+      expect(board.ships[0].hits).toBe(1);
+      board.receiveAttack(5, 6);
+      expect(board.ships[0].hits).toBe(2);
+      board.receiveAttack(5, 5);
+      expect(board.ships[0].hits).toBe(2);
+      expect(board.hitAttacks).toEqual([
+        [5, 5],
+        [5, 6],
+      ]);
+      expect(board.missedAttacks).toEqual([]);
     });
 
+    test('Given the coordinates of no ship, record the missed shot coordinates', () => {
+      const board = new Gameboard();
+
+      board.receiveAttack(5, 5);
+      expect(board.missedAttacks).toEqual([[5, 5]]);
+    });
+  });
+
+  describe('#missedAttacks', () => {
+    test('The gameboard keeps track of missed attacks correctly', () => {
+      const board = new Gameboard();
+      board.receiveAttack(1, 1);
+      board.receiveAttack(1, 2);
+      board.receiveAttack(1, 1);
+      board.receiveAttack(2, 3);
+
+      expect(board.missedAttacks).toEqual([
+        [1, 1],
+        [1, 2],
+        [2, 3],
+      ]);
+    });
+  });
+
+  describe('#allShipsSunk', () => {
+    test('Returns true if all ships have been sunk', () => {
+      const board = new Gameboard();
+      board.placeShip('Sub', 3, 5, 5, true);
+      board.receiveAttack(5,5);
+      board.receiveAttack(5,6);
+      board.receiveAttack(5,7);
+
+      expect(board.allShipsSunk()).toBe(true);
+    });
+    test('Returns false if all ships have not been sunk', () => {
+        const board = new Gameboard();
+        board.placeShip('Sub', 3, 5, 5, true);
+        expect(board.allShipsSunk()).toBe(false);
+    });
+  });
 });
