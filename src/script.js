1c357683f75f4ab5b773f3e3e6c3dd5b5008bf31
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-09-20 23:43:04 +0400

    PLACING SHIPS WORKS BABBYYYYYY

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import './styles/styles.css';
-import { Gameboard, Player } from './battleships.js';
+import "./styles/styles.css";
+import { Gameboard, Player } from "./battleships.js";
 import {
   playerTurnDiv,
   computerTurnDiv,
@@ -11,32 +11,35 @@ import {
   playerBoard,
   computerBoard,
   gridSize,
-} from './vars.js';
+} from "./vars.js";
 
 const player1board = new Gameboard(false);
 const player2board = new Gameboard(true);
 
 const player1 = new Player(playerNameInput.value, true, player1board);
-const player2 = new Player('Computer', false, player2board);
+const player2 = new Player("Computer", false, player2board);
 
-playerNameInput.value = 'Tony';
+playerNameInput.value = "Tony";
 
-startGameButton.addEventListener('click', () => {
+startGameButton.addEventListener("click", () => {
   StartGame();
 });
 
 const StartGame = () => {
-  if (playerNameInput.value == '') {
-    alert('Please enter a name');
+  if (playerNameInput.value == "") {
+    alert("Please enter a name");
     return;
   }
 
   player1.name = playerNameInput.value;
   playerNameStat.textContent = player1.name;
-  menuScreen.style.display = 'none';
-  gameScreen.style.display = 'flex';
+  menuScreen.style.display = "none";
+  gameScreen.style.display = "flex";
 
   renderBoards();
+  placeShipsRandomly(player2, true);
+
+  console.log(player2board)
 };
 
 // render boards
@@ -44,8 +47,8 @@ const StartGame = () => {
 const renderBoards = () => {
   // clear board renders
 
-  playerBoard.innerHTML = '';
-  computerBoard.innerHTML = '';
+  playerBoard.innerHTML = "";
+  computerBoard.innerHTML = "";
 
   renderBoard(player1board.board, false);
   renderBoard(player2board.board, true);
@@ -54,9 +57,9 @@ const renderBoards = () => {
 const renderBoard = (board, isComputer) => {
   for (var x = 0; x < board.length; x++) {
     for (var y = 0; y < board[x].length; y++) {
-      var square = document.createElement('div');
-      square.classList.add('square');
-      square.classList.add(isComputer ? 'computerSquare' : 'playerSquare');
+      var square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add(isComputer ? "computerSquare" : "playerSquare");
       square.dataset.x = x;
       square.dataset.y = y;
 
@@ -67,4 +70,69 @@ const renderBoard = (board, isComputer) => {
       }
     }
   }
+};
+
+// ship placement
+
+const placeShipsRandomly = (player, isComputer) => {
+  player.ships.forEach((ship, index) => {
+    const coordinates = findPossibleShipCoordinates(player.gameboard, ship);
+    let element = null;
+    let squareClass = isComputer ? "computerSquare" : "playerSquare"
+
+    for (var i = 0; i < ship.length; i++) {
+      if (coordinates[2]) {
+
+        player.gameboard.board[coordinates[0] + i][coordinates[1]] = ship.name
+        // is horizontal
+        element = document.querySelector(
+          `[data-x="${coordinates[0] + i}"][data-y="${coordinates[1]}"].${squareClass}`
+        );
+      } else {
+        player.gameboard.board[coordinates[0]][coordinates[1] + i] = ship.name
+
+        // is vertical
+        element = document.querySelector(
+          `[data-x="${coordinates[0]}"][data-y="${coordinates[1] + i}"].${squareClass}`
+        );
+      }
+      // element.classList.add("red");
+    }
+  });
+};
+
+const findPossibleShipCoordinates = (board, ship) => {
+  var freePositionFound = false;
+  let x = 0;
+  let y = 0;
+  var isHorizontal = false;
+
+  while (!freePositionFound) {
+    var isHorizontal = Math.round(Math.random() * 10) > 5 ? true : false;
+    freePositionFound = true;
+    x = Math.floor(Math.random() * gridSize);
+    y = Math.floor(Math.random() * gridSize);
+
+    if (isHorizontal) {
+      x > gridSize - ship.length ? (x = x - ship.length) : (x = x);
+
+      for (var i = 0; i < ship.length; i++) {
+        if (board.board[x + i][y] !== "Empty") {
+          freePositionFound = false;
+        }
+      }
+    } else {
+      y > gridSize - ship.length ? (y = y - ship.length) : (y = y);
+
+      for (var i = 0; i < ship.length; i++) {
+        if (board.board[x][y + i] !== "Empty") {
+          freePositionFound = false;
+        }
+      }
+    }
+  }
+
+
+
+  return [x, y, isHorizontal];
 };
